02394b3e25d395795463d329d0baf89187e4bf6f
KalebArmbrust <kalebarmbrust@gmail.com>
2023-03-28 21:55:02 -0500

    Basic Layout

--- src/App.vue
+++ src/App.vue
@@ -9,16 +9,6 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-}
 
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,24 @@
 <script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
+
 </script>
 
 <template>
-  <div class="flex h-screen w-full bg-[url('../src/assets/Coding-Challenge-Image.png')] bg-cover bg-center from-[#611818]">
+  <div class="flex h-screen w-full bg-[url('../src/assets/Coding-Challenge-Image.png')] bg-cover bg-center static">
+    <div class="bg-gradient-to-r from-[#611818] to-[#611818] min-w-[400px]"></div>
     <div>
-      <h1 class="Title">Explore</h1>
-      <div grid grid-cols-2 gap-4>
+      <h1 class="absolute bottom-20 text-[#FFFFFF] font-['Didot'] text-[224px] font-bold">Explore</h1>
+      <div class="absolute bottom-0 flex flex-row">
         <img src="../assets/Plus.png" alt="Plus Sign">
-        <h3>More Details</h3>        
+        <h3 class="text-[#FFFFFF] font-['Lato'] text-[25px] font-normal">More Details</h3>
       </div>
     </div>
     <div>
-      <img src="../assets/Facebook.svg" alt="Facebook Icon">
-      <img src="../assets/Instagram.svg" alt="Instagram Icon">
+        <img class="absolute bottom-20 right-10" src="../assets/Facebook.svg" alt="Facebook Icon">
+        <img class="absolute bottom-1 right-9" src="../assets/Instagram.svg" alt="Instagram Icon">
     </div>
   </div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
+
 </style>
