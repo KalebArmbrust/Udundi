e84e987d8221439f402394c10b0a84fb619f0336
KalebArmbrust <kalebarmbrust@gmail.com>
2023-03-29 15:50:43 -0500

    All done but the transition and responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,28 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+        keyframes: {
+            'fade-in-down': {
+                '0%': {
+                    opacity: '0',
+                    transform: 'translateY(-50px)'
+                },
+                '100%': {
+                    opacity: '1',
+                    transform: 'translateY(0)'
+                },
+            }
+        },
+        animation: {
+            'fade-in-down': 'fade-in-down 2s ease-out'
+        },
+        fontFamily: {
+          'Lato': ['Lato']
+        },
+    },
+},
+variants: {},
+plugins: [],
 }
 
