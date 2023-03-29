9989821e0a596a3168a95340cfe07505ffa70bc5
KalebArmbrust <kalebarmbrust@gmail.com>
2023-03-28 21:06:50 -0500

    Background Image and Content

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,13 +8,18 @@ defineProps({
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+  <div class="flex h-screen w-full bg-[url('../src/assets/Coding-Challenge-Image.png')] bg-cover bg-center from-[#611818]">
+    <div>
+      <h1 class="Title">Explore</h1>
+      <div grid grid-cols-2 gap-4>
+        <img src="../assets/Plus.png" alt="Plus Sign">
+        <h3>More Details</h3>        
+      </div>
+    </div>
+    <div>
+      <img src="../assets/Facebook.svg" alt="Facebook Icon">
+      <img src="../assets/Instagram.svg" alt="Instagram Icon">
+    </div>
   </div>
 </template>
 
@@ -23,21 +28,5 @@ h1 {
   font-weight: 500;
   font-size: 2.6rem;
   top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
 }
 </style>
